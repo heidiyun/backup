8fccbd8a75ac4b78f5dd5b17c0bb329c2592cc8b
Heidi YunJeong Goo <heidiyun.goo@gmail.com>
2018-08-26 21:09:07 +0900

    ideaprojects delete

--- IdeaProjects/Program_0424/src/Util.java
+++
@@ -1,6 +0,0 @@
-public class Util implements Constants {
-    public static Vector2 IndexToPosition(int row_index, int column_index) {
-        return new Vector2(column_index * BLOCK_WIDTH + MARGIN_HORIZONTAL + BLOCK_WIDTH / 2,
-                row_index * BLOCK_HEIGHT + MARGIN_VERTICAL + 50 + BLOCK_HEIGHT / 2);
-    }
-}
--- IdeaProjects/Sample/src/Example2.kt
+++
@@ -1,9 +0,0 @@
-package geometry.example
-
-import geometry.shapes.Rectangle
-
-fun main(args: Array<String>) {
-    val rectangle = Rectangle(100,120)
-    println(rectangle.isSquare)
-}
-
